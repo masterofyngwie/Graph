48a1c4aacd3ba284c5f4b50cd409368da4a81a83
Jorge <jorgemanzanarez@imac-de-jorge.local>
2018-09-23 21:52:58 -0500

    Initial Commit

---
+++ Graph/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Graph
+//
+//  Created by Jorge on 23/09/18.
+//  Copyright © 2018 Jorge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
